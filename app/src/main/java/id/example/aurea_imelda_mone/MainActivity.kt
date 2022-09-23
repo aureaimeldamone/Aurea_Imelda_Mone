581224ea2819e6f76cd5726c78285d7d0aba6899
aureaimeldamone <90777771+aureaimeldamone@users.noreply.github.com>
2022-09-22 20:36:34 -1200

    Initial commit

---
+++ app/src/main/java/id/example/aurea_imelda_mone/MainActivity.kt
@@ -0,0 +1,11 @@
+ package id.example.aurea_imelda_mone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
